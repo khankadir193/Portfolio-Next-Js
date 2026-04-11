9c52bd05fc88bc4c029e62efc7ee6f644f7e26fc
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2026-04-11 19:30:46 +0530

    project data

--- app/data/projectsData.js
+++ app/data/projectsData.js
@@ -1,24 +1,27 @@
 // Projects data
 export const projectsData = [
   {
-    title: 'Master Of Computer Applications',
-    description: 'XYZ University - 2020 - June 2022. New York, NY - New York, NY.',
-    technologies: ['React', 'Next.js', 'Firebase', 'Redux'],
-    link: '#',
-    icon: '📱'
+    title: 'Quiz Application',
+    description: 'In this project, I\'ve created a React JS quiz that covers both basic and advanced levels of knowledge. It\'s designed for individuals who want to assess their understanding of React. By taking this quiz, users can evaluate their proficiency in React JS and gain insights into their knowledge level.',
+    technologies: ['React Js', 'Redux', 'BootStrap', 'HTML', 'CSS', 'JavaScript'],
+    link: 'https://reactbrainchallenges.netlify.app/',
+    // icon: '??',
+    period: 'Jun 2023 - July 2023'
   },
   {
-    title: 'CRM Application',
-    description: 'Developed integrate and versatile CURL for managing a major engaging effective of business. Maintained improved execution services through multiple states.',
-    technologies: ['React', 'Next.js', 'Firebase', 'Redux'],
-    link: '#',
-    icon: '💼'
+    title: 'FitNess App',
+    description: 'This application is dedicated to promoting fitness and well-being by providing valuable guidance on proper exercise techniques. It offers a diverse range of exercises that cater to various fitness levels and goals, making it an essential resource for everyone looking to improve their health and physical fitness.',
+    technologies: ['React Js', 'Redux', 'BootStrap', 'HTML', 'CSS', 'JavaScript', 'MaterialUI', 'RapidApi'],
+    link: 'http://liftlifefitness.netlify.app/',
+    // icon: '??',
+    period: 'Jun 2023 - July 2023'
   },
   {
     title: 'Portfolio Website',
-    description: 'Created a full post mUTube s 3 will integrative solution for ustre segment, and commerce ingentive openeloyment. Mained a vadssta red wittd contort, sercipt depne reling sutablassbtent, and miruds.',
-    technologies: ['Next.js', 'Tailwind CSS', 'Framer Motion'],
-    link: '#',
-    icon: '🎨'
+    description: 'A responsive personal portfolio website built with React Js and Material UI, showcasing skills, experience, and projects.',
+    technologies: ['React.js', 'Material UI', 'JavaScript', 'HTML', 'CSS'],
+    link: 'https://abdulkadirkhan786.netlify.app/',
+    // icon: '??',
+    period: 'Aug 2023 - OCT 2023'
   }
 ];
