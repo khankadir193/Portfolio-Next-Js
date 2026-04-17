b2d55dccc0473a040cb75185eb166f92dcd3a177
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2026-04-17 12:29:53 +0530

    hydration issues has been resolved

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>
+      <body suppressHydrationWarning={true}>
         <MuiThemeProvider>
           <AmbientBackground />
           <BackgroundName />
--- next.config.js
+++ next.config.js
@@ -9,23 +9,11 @@ const nextConfig = {
   },
   poweredByHeader: false,
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-  },
-  headers: async () => [
-    {
-      source: '/_next/static/(.*)',
-      headers: [
-        {
-          key: 'Cache-Control',
-          value: 'public, max-age=31536000, immutable'
-        }
-      ]
-    }
-  ]
+  }
 }
 
 module.exports = nextConfig
